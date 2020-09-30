27111bcde8c9a56272b2c278b435a612779b8c06
Akutchi <diskoflife@gmail.com>
2020-09-30 23:17:50 +0200

    Update charList.js

--- charList.js
+++ charList.js
@@ -28,6 +28,7 @@ xhr.onload = function () {
             
             for(var i = 0; i < 3; i++){
                 var v = u[i].split(" ");
+                console.log(v);
                 charList.push({kanji:v[0], kun:v[1], on:v[2], meaning:v[3]});
     
             }
